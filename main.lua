66ddf889f73de2841891d115cd02e4b177194b8e
doink <aricjacob123@gmail.com>
2025-12-26 19:33:13 -0600

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,6 +1,6 @@
 if game.PlaceId == 114234929420007 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/d0inks/moon.lua/refs/heads/main/bloxstrike.lua"))() --bloxstrike
-elseif game.PlaceId == 301549746 then
+elseif game.PlaceId == 301549746 or game.PlaceId == 1480424328 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/d0inks/moon.lua/refs/heads/main/counterblox.lua"))() -- counterblox
 else
     loadstring(game:HttpGet("https://raw.githubusercontent.com/d0inks/moon.lua/refs/heads/main/universal.lua"))() --universal
